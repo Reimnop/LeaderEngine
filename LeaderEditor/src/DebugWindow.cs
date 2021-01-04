9c150fd79ec84692a94e5d31720d2a8c6d4e0d81
UserTCQ <rayngu2005@gmail.com>
2021-01-04 08:53:47 +0700

    Update DebugWindow.cs

--- LeaderEditor/src/DebugWindow.cs
+++ LeaderEditor/src/DebugWindow.cs
@@ -19,8 +19,9 @@ namespace LeaderEditor
             if (IsOpen)
                 if (ImGui.Begin("Debug"))
                 {
-                    ImGui.Text("FPS: " + MathF.Round(100.0f / Time.deltaTime) / 100.0f);
-                    ImGui.Text("Frametime: " + MathF.Round(Time.deltaTime * 100000.0f) / 100.0f + "ms");
+                    ImGui.Text("FPS: " + MathF.Round(100.0f / Time.deltaTimeUnscaled) / 100.0f);
+                    ImGui.Text("Frametime: " + MathF.Round(Time.deltaTimeUnscaled * 100000.0f) / 100.0f + "ms");
+                    ImGui.SliderFloat("Time scale", ref Time.timeScale, 0.0f, 10.0f);
                     ImGui.End();
                 }
         }
