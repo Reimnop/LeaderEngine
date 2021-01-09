94900a49b283e8a46509657879a14198e52dabd1
UserTCQ <rayngu2005@gmail.com>
2021-01-09 21:13:06 +0700

    Update DirectionalLight.cs

--- LeaderEngine/src/Types/Components/Rendering/DirectionalLight.cs
+++ LeaderEngine/src/Types/Components/Rendering/DirectionalLight.cs
@@ -11,7 +11,7 @@ namespace LeaderEngine
 
         public void GenViewProject(out Matrix4 view, out Matrix4 proj)
         {
-            proj = Matrix4.CreateOrthographic(128.0f, 128.0f, -64.0f, 64.0f);
+            proj = Matrix4.CreateOrthographic(16.0f, 16.0f, -8.0f, 8.0f);
 
             view = Matrix4.CreateFromQuaternion(Quaternion.FromEulerAngles(-new Vector3(
                 MathHelper.DegreesToRadians(transform.RotationEuler.X),
