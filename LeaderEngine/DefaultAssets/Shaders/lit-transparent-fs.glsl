49aa40c0c7ae3819b0b7e0fc964c056fd383dba0
UserTCQ <rayngu2005@gmail.com>
2021-01-15 08:44:45 +0700

    Added shaders maybe

---
+++ LeaderEngine/DefaultAssets/Shaders/lit-transparent-fs.glsl
@@ -0,0 +1,39 @@
+﻿#version 400 core
+
+layout (location = 0) out vec4 gAlbedoSpec;
+layout (location = 1) out vec3 gPosition;
+layout (location = 2) out vec3 gNormal;
+layout (location = 3) out vec3 gPositionViewSpace;
+layout (location = 4) out vec3 gNormalViewSpace;
+
+uniform bool useTexture;
+
+uniform vec4 color;
+
+uniform sampler2D texture0;
+
+in vec3 VertCol;
+in vec2 TexCoord;
+
+in vec3 NormalWorldSpace;
+in vec3 FragPosWorldSpace;
+
+in vec3 NormalViewSpace;
+in vec3 FragPosViewSpace;
+
+in vec3 FragPos;
+
+void main() 
+{
+	vec4 outColor = vec4(VertCol, 1.0) * color;
+
+	if (useTexture)
+		outColor *= texture(texture0, TexCoord);
+
+	gAlbedoSpec = outColor;
+	gPosition = FragPosWorldSpace;
+	gNormal = normalize(NormalWorldSpace);
+
+	gPositionViewSpace = FragPosViewSpace;
+	gNormalViewSpace = NormalViewSpace;
+}
---
+++ LeaderEngine/DefaultAssets/Shaders/lit-transparent-vs.glsl
@@ -0,0 +1,37 @@
+﻿#version 400 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aVertCol;
+layout (location = 2) in vec3 aNormal;
+layout (location = 3) in vec2 aTexCoord;
+
+out vec3 VertCol;
+
+out vec2 TexCoord;
+
+out vec3 NormalWorldSpace;
+out vec3 FragPosWorldSpace;
+
+out vec3 NormalViewSpace;
+out vec3 FragPosViewSpace;
+
+uniform mat4 mvp;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main() 
+{
+	VertCol = aVertCol;
+
+	TexCoord = aTexCoord;
+
+	NormalWorldSpace = aNormal * mat3(model);
+	FragPosWorldSpace = vec3(vec4(aPos, 1.0) * model);
+
+	NormalViewSpace = vec3(vec4(aNormal * mat3(model), 1.0) * view);
+	FragPosViewSpace = vec3(vec4(aPos, 1.0) * model * view);
+
+	gl_Position = vec4(aPos, 1.0) * mvp;
+}
