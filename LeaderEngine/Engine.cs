7e71b0f3436cfa06cad98200e23b3ef78a6e5e7c
UserTCQ <rayngu2005@gmail.com>
2021-04-03 00:37:29 +0700

    Added timing

--- LeaderEngine/Engine.cs
+++ LeaderEngine/Engine.cs
@@ -4,6 +4,14 @@ using OpenTK.Windowing.Desktop;
 
 namespace LeaderEngine
 {
+    public static class Time
+    {
+        public static float Elapsed { get; internal set; }
+        public static float DeltaTime { get; internal set; }
+        public static float UnscaledDeltaTime { get; internal set; }
+        public static float TimeScale = 1.0f;
+    }
+
     public static class Engine
     {
         public static GameWindow MainWindow { get; private set; }
@@ -22,6 +30,13 @@ namespace LeaderEngine
 
         private static void UpdateFrame(FrameEventArgs obj)
         {
+            //updates time
+            float t = (float)obj.Time;
+
+            Time.Elapsed += t;
+            Time.DeltaTime = t * Time.TimeScale;
+            Time.UnscaledDeltaTime = t;
+
             //updates entire scene hierachy
             DataManager.CurrentScene.RootEntity.Update();
         }
