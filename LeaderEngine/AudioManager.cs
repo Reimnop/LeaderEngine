ae493dd5142bbe9f49d22f650a0e6c1384a97186
Reimnop <56994757+Reimnop@users.noreply.github.com>
2021-04-15 13:08:52 +0700

    Installed OpenAL.

--- LeaderEngine/AudioManager.cs
+++ LeaderEngine/AudioManager.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using OpenTK.Audio.OpenAL;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LeaderEngine
 {
+    public class AudioClip
+    {
+        private AudioClip()
+        {
+
+        }
+
+        public static AudioClip FromFile(string path)
+        {
+            return new AudioClip();
+        }
+    }
+
     public class AudioSource
     {
 
@@ -11,6 +25,37 @@ namespace LeaderEngine
 
     public static class AudioManager
     {
+        private static Dictionary<string, AudioSource> audioSources = new Dictionary<string, AudioSource>();
 
+        internal static void Init()
+        {
+            string deviceName = ALC.GetString(ALDevice.Null, AlcGetString.DefaultDeviceSpecifier);
+            var device = ALC.OpenDevice(deviceName);
+            var context = ALC.CreateContext(device, (int[])null);
+            ALC.MakeContextCurrent(context);
+
+            CheckALError("Start");
+
+            Logger.Log("Audio initialized.");
+        }
+
+        internal static void CheckALError(string str)
+        {
+            ALError error = AL.GetError();
+            if (error != ALError.NoError)
+            {
+                Console.WriteLine($"ALError at '{str}': {AL.GetErrorString(error)}");
+            }
+        }
+
+        public static AudioSource GetAudioSource(string id)
+        {
+            if (audioSources.TryGetValue(id, out AudioSource source))
+                return source;
+
+            AudioSource audioSource = new AudioSource();
+            audioSources.Add(id, audioSource);
+            return audioSource;
+        }
     }
 }
