2f7af4bfe224c7014f6b77b047eec68eafa3b977
UserTCQ <rayngu2005@gmail.com>
2021-04-21 22:00:05 +0700

    Added serialization of meshes

--- LeaderEngine/EntityComponentSystem/Entity.cs
+++ LeaderEngine/EntityComponentSystem/Entity.cs
@@ -1,7 +1,6 @@
 ﻿using OpenTK.Mathematics;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace LeaderEngine
 {
--- LeaderEngine/Rendering/Mesh.cs
+++ LeaderEngine/Rendering/Mesh.cs
@@ -1,6 +1,7 @@
 ﻿using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
 using System;
+using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -40,11 +41,112 @@ namespace LeaderEngine
 
     public sealed class Mesh : IDisposable
     {
-        public int VerticesCount { get; private set; }
-        public int IndicesCount { get; private set; }
-        public PrimitiveType PrimitiveType { get; private set; }
+        private interface IVertexArray
+        {
+            public Type VertexType { get; }
 
+            public int VerticesCount { get; }
+            public int IndicesCount { get; }
+
+            public void Serialize(Stream stream);
+            public void Deserialize(Stream stream);
+        }
+
+        private struct VertexArrayGeneric<T> : IVertexArray where T : struct
+        {
+            private T[] vertices;
+            private uint[] indices;
+
+            public Type VertexType { get; private set; }
+
+            public int VerticesCount => vertices.Length;
+            public int IndicesCount => indices.Length;
+
+            public VertexArrayGeneric(T[] vertices, uint[] indices)
+            {
+                VertexType = typeof(T);
+
+                this.vertices = vertices;
+                this.indices = indices;
+            }
+
+            public void Serialize(Stream stream)
+            {
+                int vSize = Unsafe.SizeOf<T>();
+
+                //write length
+                stream.Write(BitConverter.GetBytes(vertices.Length * vSize));
+                IntPtr vPtr = Marshal.AllocHGlobal(vSize);
+                byte[] vBuffer = new byte[vSize];
+                //write vertices
+                for (int i = 0; i < vertices.Length; i++)
+                {
+                    T vertex = vertices[i];
+
+                    Marshal.StructureToPtr(vertex, vPtr, true);
+                    Marshal.Copy(vPtr, vBuffer, 0, vSize);
+
+                    stream.Write(vBuffer, 0, vSize);
+                }
+                Marshal.FreeHGlobal(vPtr);
+
+                //write length
+                stream.Write(BitConverter.GetBytes(indices.Length * sizeof(uint)));
+                //write indices
+                for (int i = 0; i < indices.Length; i++)
+                {
+                    stream.Write(BitConverter.GetBytes(indices[i]));
+                }
+            }
+            public void Deserialize(Stream stream)
+            {
+                //read length
+                byte[] buffer = new byte[sizeof(int)];
+                stream.Read(buffer, 0, sizeof(int));
+
+                int vBufSize = BitConverter.ToInt32(buffer, 0);
+                int vSize = Unsafe.SizeOf<T>();
+                int vLength = vBufSize / vSize;
+
+                //read vertices
+                vertices = new T[vLength];
+                buffer = new byte[vSize];
+                GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+                for (int i = 0; i < vLength; i++)
+                {
+                    stream.Read(buffer, 0, vSize);
+                    vertices[i] = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+                }
+                handle.Free();
+
+                //read length
+                buffer = new byte[sizeof(int)];
+                stream.Read(buffer, 0, sizeof(int));
+
+                int iBufSize = BitConverter.ToInt32(buffer, 0);
+                int iSize = sizeof(uint);
+                int iLength = iBufSize / iSize;
+
+                //read indices
+                indices = new uint[iLength];
+                buffer = new byte[iSize];
+                handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+                for (int i = 0; i < iLength; i++)
+                {
+                    stream.Read(buffer, 0, iSize);
+                    indices[i] = BitConverter.ToUInt32(buffer, 0);
+                }
+                handle.Free();
+            }
+        }
+
+        //vertex data
         private int VAO, VBO, EBO;
+        private IVertexArray vertexArray;
+        public int VerticesCount => vertexArray.VerticesCount;
+        public int IndicesCount => vertexArray.IndicesCount;
+
+        public PrimitiveType PrimitiveType { get; private set; }
 
         public readonly string Name;
 
@@ -66,9 +168,8 @@ namespace LeaderEngine
         {
             PrimitiveType = primitiveType;
 
-            //update vertices and indices counts
-            VerticesCount = vertices.Length;
-            IndicesCount = indices.Length;
+            //store vertices
+            vertexArray = new VertexArrayGeneric<T>(vertices, indices);
 
             GL.BindVertexArray(VAO);
 
@@ -104,12 +205,12 @@ namespace LeaderEngine
 
         public void UpdateMesh<T>(T[] vertices, uint[] indices) where T : struct
         {
+            //store vertices
+            vertexArray = new VertexArrayGeneric<T>(vertices, indices);
+
             //upload buffers
             GL.NamedBufferData(VBO, vertices.Length * Unsafe.SizeOf<T>(), vertices, BufferUsageHint.DynamicCopy);
             GL.NamedBufferData(EBO, indices.Length * sizeof(uint), indices, BufferUsageHint.DynamicCopy);
-
-            VerticesCount = vertices.Length;
-            IndicesCount = indices.Length;
         }
 
         public void Use()
